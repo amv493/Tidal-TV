fa2d0b47f58cdc27f97e73590eb0fd9188b6941f
Anthony McKinney <amv493@gmail.com>
2025-12-26 16:56:31 -0500

    First Commit. Project Setup

---
+++ src/data/shows.js
@@ -0,0 +1,20 @@
+const shows = [
+    {
+        id: 1, 
+        title: "One Piece", 
+        episodes: [
+            { id: "e1", title: "", url: ""}, 
+            { id: "e2", title: "", url: ""}
+        ]
+    }, 
+    {
+        id: 1, 
+        title: "Avatar: The Last Airbender", 
+        episodes: [
+            { id: "e1", title: "", url: ""}, 
+            { id: "e2", title: "", url: ""}
+        ]
+    }
+];
+
+export default shows;
